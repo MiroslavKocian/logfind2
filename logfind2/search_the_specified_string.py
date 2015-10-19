9839ed648a35d4756338a67ecb5f853ea1618e1f
Miroslav Kocian <mirkocian@gmail.com>
2015-10-19 10:47:09 +0200

    fixed test_empty_single_space_string() and test_one_character_string()

--- logfind2/search_the_specified_string.py
+++ logfind2/search_the_specified_string.py
@@ -1,5 +1,5 @@
 from sys import argv
 
-def search_the_specified_string():
+def search_the_specified_string(argv):
     specified_string = "xargs -0 grep -H -s " + ' '.join([str(item) for item in argv[1:]]) 
-    print specified_string
+    return specified_string
--- tests/search_the_specified_string_tests.py
+++ tests/search_the_specified_string_tests.py
@@ -2,10 +2,10 @@ from nose.tools import *
 from logfind2.search_the_specified_string import *
 from sys import argv
 
-def test_empty_string():
-    specified_string = "xargs -0 grep -H -s " + ' '.join([str(item) for item in argv[1:]])
-    assert_equal("xargs -0 grep -H -s ", specified_string)
+def test_empty_single_space_string():
+    argv = " "
+    assert_equal("xargs -0 grep -H -s ", search_the_specified_string(argv))
 
-def test_short_string():
-    specified_string = "xargs -0 grep -H -s b" + ' '.join([str(item) for item in argv[1:]])
-    assert_equal("xargs -0 grep -H -s b", specified_string)
+def test_one_character_string():
+    argv = ["logfind2", "b"]
+    assert_equal("xargs -0 grep -H -s b", search_the_specified_string(argv))
