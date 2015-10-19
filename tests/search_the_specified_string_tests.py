6ca50b772d943d81bde1f79560e0a9c572d9612a
Miroslav Kocian <mirkocian@gmail.com>
2015-10-19 10:27:31 +0200

    fixed test_empty_string() and test_short_string()

--- tests/search_the_specified_string_tests.py
+++ tests/search_the_specified_string_tests.py
@@ -4,8 +4,8 @@ from sys import argv
 
 def test_empty_string():
     specified_string = "xargs -0 grep -H -s " + ' '.join([str(item) for item in argv[1:]])
-    assert_equal(specified_string, "xargs -0 grep -H -s ")
+    assert_equal("xargs -0 grep -H -s ", specified_string)
 
 def test_short_string():
-    specified_string = "xargs -0 grep -H -s " + ' '.join([str(item) for item in argv[1:]])
-    assert_equal(specified_string, "xargs -0 grep -H -s b")
+    specified_string = "xargs -0 grep -H -s b" + ' '.join([str(item) for item in argv[1:]])
+    assert_equal("xargs -0 grep -H -s b", specified_string)
